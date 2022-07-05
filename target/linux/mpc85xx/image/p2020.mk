d64c87818a7ad5ee554724cc0caced3a77d87687
Josef Schlehofer <pepe.schlehofer@gmail.com>
2022-07-05 09:38:02 +0200

    p2020: add kmod-crypto-hw-talitos
    Signed-off-by: Josef Schlehofer <pepe.schlehofer@gmail.com>

--- target/linux/mpc85xx/image/p2020.mk
+++ target/linux/mpc85xx/image/p2020.mk
@@ -3,7 +3,7 @@ define Device/freescale_p2020rdb
   DEVICE_MODEL := P2020RDB
   DEVICE_DTS_DIR := $(DTS_DIR)/fsl
   DEVICE_PACKAGES := kmod-hwmon-lm90 kmod-rtc-ds1307 \
-	kmod-gpio-pca953x kmod-eeprom-at24
+	kmod-gpio-pca953x kmod-eeprom-at24 kmod-crypto-hw-talitos
   BLOCKSIZE := 128k
   KERNEL := kernel-bin | gzip | \
 	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
